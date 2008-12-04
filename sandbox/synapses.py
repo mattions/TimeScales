7c2c17242b86e3036a3550f253131f0fcca3215f
mattioni <mattioni@47c4ba39-c54e-0410-9af8-a8dc67f891c1>
2008-12-04 17:19:48 +0000

    input graph added
    git-svn-id: https://svn.ebi.ac.uk/compneur/users/mattioni/Time_scales/trunk@517 47c4ba39-c54e-0410-9af8-a8dc67f891c1

--- sandbox/synapses.py
+++ sandbox/synapses.py
@@ -13,7 +13,7 @@ soma.connect(dend)
 
 stim = h.NetStim()
 
-stim.number = 50
+stim.number = 10
 stim.start = 5
 stim.noise = 0
 
@@ -21,24 +21,25 @@ def insertTestSyn():
     syn = h.Exp2Syn(0.5, sec = dend)
     syn.tau1 = 3 # rise time
     syn.tau2 = 15 # decay time
-    syn.e = -60 # reversal potential
+    syn.e = 0 # reversal potential
     return syn
 
 def insertAMPASyn():
     ampa = h.AMPA(0.5, sec = dend)
     return ampa
 
-#syn = insertTestSyn()
-ampa = insertAMPASyn()
+syn = insertTestSyn()
+#ampa = insertAMPASyn()
 
-nc = h.NetCon(stim, ampa)
-nc.weight[0] = 1
-input_vec = h.Vector()
+nc = h.NetCon(stim, syn)
+nc.weight[0] = 0.01
+inputs_vec = h.Vector()
+nc.record(inputs_vec)
 
 
 # vector
-t = h.Vector()
-t.record(h._ref_t)
+t_vec = h.Vector()
+t_vec.record(h._ref_t)
 
 v_soma_vec = h.Vector()
 v_soma_vec.record(soma(0.5)._ref_v)
@@ -76,13 +77,36 @@ run (1000, initialized)
 #=+++++++++ Plotting ++++++++=#
 
 # Transforming the vecotr in numpy
+t = numpy.array(t_vec)
+t = numpy.round(t, decimals=9) # round
+
 v_soma = numpy.array(v_soma_vec)
 v_dend = numpy.array(v_dend_vec)
+inputs = numpy.array(inputs_vec)
 
+
+
+def inputsGraph(t, inputs):
+    
+    inputs_event = numpy.zeros_like(t)
+    for indx, x in enumerate (t):
+        for y in inputs:
+            if x == y:
+                inputs_event[indx] = 1
+    masked = numpy.ma.masked_where(inputs_event == 0, inputs_event)
+    #figure()
+    plot(t, masked, 'ro')
+
+    
 #plot
+figure()
 plot(t,v_soma, label = "soma")
+legend()
+figure()
 plot(t,v_dend, label = "dend")
 legend()
+
+inputsGraph(t, inputs)
 show()
 
 
