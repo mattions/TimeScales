4a7ba38ee421177f9e2c4b50a30ded621210e306
mattioni <mattioni@47c4ba39-c54e-0410-9af8-a8dc67f891c1>
2009-01-25 11:44:13 +0000

    changed the initial file. no synapses created. Same result as when created with the python only. Maybe it's the synapse effect
    git-svn-id: https://svn.ebi.ac.uk/compneur/users/mattioni/Time_scales/trunk@787 47c4ba39-c54e-0410-9af8-a8dc67f891c1

--- init.py
+++ init.py
@@ -17,17 +17,17 @@ t.record(h._ref_t)
 print "Created vectors t"
 
 v_soma = h.Vector()
-v_soma.record(h.MSP_Cell[0].soma(0.5)._ref_v)
+v_soma.record(h.MSN[0].soma(0.5)._ref_v)
 print "Created vectors v_soma"
 
 ca_in_s = h.Vector()
-ca_in_s.record(h.MSP_Cell[0].soma(0.5)._ref_cai)
+ca_in_s.record(h.MSN[0].soma(0.5)._ref_cai)
 print "Created vectors ca_in_s"
 
 ca_in_d = h.Vector()
-ca_in_d.record(h.MSP_Cell[0].dend1_1[0](0.5)._ref_cai)
+ca_in_d.record(h.MSN[0].dend1_1[0](0.5)._ref_cai)
 
 
 import neuron.gui
-h.load_file("guiRig.ses")
+#h.load_file("guiRig.ses")
 
