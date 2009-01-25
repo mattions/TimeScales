9eb2b9347f4d267657ff579a44b5bbf9e1de1427
mattioni <mattioni@47c4ba39-c54e-0410-9af8-a8dc67f891c1>
2009-01-25 11:44:19 +0000

    Added the weight from the ampa channel taken from Grunditz et al. It works
    git-svn-id: https://svn.ebi.ac.uk/compneur/users/mattioni/Time_scales/trunk@789 47c4ba39-c54e-0410-9af8-a8dc67f891c1

--- sandbox/synapses.py
+++ sandbox/synapses.py
@@ -13,8 +13,9 @@ def insertTestSyn():
     return syn
 
 def insertAMPASyn():
-    ampa = h.AMPA(0.5, sec = dend)
+    ampa = h.ampa(0.5, sec = dend)
     return ampa
+
 def insertMyExp2Syn():
     syn = h.my_exp2syn(0.5, sec = dend)
     print "Inserted syn: tau1 %f tau2 %f e %f" %(syn.tau1, syn.tau2, syn.e)
@@ -33,34 +34,58 @@ def run(interval):
         
         
 # run
-def testExperiment():
+def testExperiment(weight=100):
     initialize()
     run (50)
     # change the weight of the synapses
-    nc.weight[0] = 0.1
+    nc.weight[0] = weight
     run(50)
+
+# Vecs
+def createVectors():
+    # vector
+    vecs = {}
+    vecs["t"] = h.Vector()
+    vecs["t"].record(h._ref_t)
+    
+    vecs["v_soma"] = h.Vector()
+    vecs["v_soma"].record(soma(0.5)._ref_v)
+    
+    vecs["v_dend"] = h.Vector()
+    vecs["v_dend"].record(dend(0.5)._ref_v)
+    
+    vecs["syn_i"] = h.Vector()
+    vecs["syn_i"].record(syn._ref_i)
+    
+    return vecs
 
 #=+++++++++ Plotting ++++++++=#
 
-def plotVecs():
+def plotVecs(vecs_dict):
     """Transform the vectors in numpy array and display it"""
     
     
     # Transforming the vecotr in numpy
-    t = numpy.array(t_vec)
+    t = numpy.array(vecs_dict["t"])
     t = numpy.round(t, decimals=9) # round
     
-    v_soma = numpy.array(v_soma_vec)
-    v_dend = numpy.array(v_dend_vec)
-    inputs = numpy.array(inputs_vec)
+    v_soma = numpy.array(vecs_dict["v_soma"])
+    v_dend = numpy.array(vecs_dict["v_dend"])
+    inputs = numpy.array(vecs_dict["stimul"])
     #plot
     figure()
     plot(t,v_soma, label = "soma")
     legend(loc = 'lower right')
+    
     figure()
     plot(t,v_dend, label = "dend")
     inputsGraph(t, inputs)
     legend(loc = 'lower right')
+    
+    figure()
+    plot(t, vecs_dict["syn_i"], label="synaptical current")
+    legend()
+    
     show()
 
 def inputsGraph(t, inputs):
@@ -92,29 +117,18 @@ if __name__ == "__main__":
     stim.number = 10
     stim.start = 5
     stim.noise = 0
-    syn = insertTestSyn()
-    #syn = insertAMPASyn()
+    #syn = insertTestSyn()
+    syn = insertAMPASyn()
     #syn = insertMyExp2Syn()
+    
+    vecs = createVectors()
 
     nc = h.NetCon(stim, syn)
-    nc.weight[0] = 0.01
-    inputs_vec = h.Vector()
-    nc.record(inputs_vec)
-    
-    
-    # vector
-    t_vec = h.Vector()
-    t_vec.record(h._ref_t)
-    
-    v_soma_vec = h.Vector()
-    v_soma_vec.record(soma(0.5)._ref_v)
-    
-    
-    v_dend_vec = h.Vector()
-    v_dend_vec.record(dend(0.5)._ref_v)
-    
+    nc.weight[0] = 10
+    vecs["stimul"] = h.Vector()
+    nc.record(vecs["stimul"])
     
     testExperiment()
-    plotVecs()
+    plotVecs(vecs)
 
 
