ca9f8ad0851b7a558715d69763e358119282074b
mattioni <mattioni@47c4ba39-c54e-0410-9af8-a8dc67f891c1>
2009-03-04 16:50:15 +0000

    reformatted the code of the wrapper in a class format
    git-svn-id: https://svn.ebi.ac.uk/compneur/users/mattioni/Time_scales/trunk@1066 47c4ba39-c54e-0410-9af8-a8dc67f891c1

--- sandbox/odeSolverWrapper.py
+++ sandbox/odeSolverWrapper.py
@@ -6,213 +6,187 @@ import ctypes as CT
 import numpy
 import pylab
 
-# in theory this whole thing should be platform independent
-# note: Change the name of the library
-if os.sys.platform == 'win32':
-    print 'Opening windows.'
-    sbmlOdeSolver = CT.CDLL('c://windows//system32//libODES.dll')
-# Also available for MacOS although I don't know all the details.
-# if you are using a Mac please check the results of 'os.sys.platform'
-# and set your absolute path to libsbml3 and let me know if it works
-elif os.sys.platform == 'darwin' or os.name == 'mac':
-    print 'Here be apples.'
-    sbmlOdeSolver = CT.CDLL('/usr/lib/libODES.dylib', CT.RTLD_GLOBAL)
-else:
-    print 'Here be penguins.'
-    sbmlOdeSolver = CT.CDLL('/usr/local/lib/libODES.so')
-    
 
-########################################################
-
-def setReturnType(): 
-    """Set the return type in an appropriate way"""
+class SOSWrap:
+    """Wrapper class built around the SBMLOdeSolver Library"""
     
-    sbmlOdeSolver.TimeCourse_getName.restype = CT.c_char_p # Return a string
-    sbmlOdeSolver.TimeCourse_getValue.restype = CT.c_double # Return a double
-    sbmlOdeSolver.IntegratorInstance_getVariableValue.restype = CT.c_double
-    sbmlOdeSolver.IntegratorInstance_getTime.restype = CT.c_double
-
-def loadModel(filename):
-    """Parse the SBML model
-    return the doc handle to the model"""
-    # Parsing the model with sbml library
-    sbmlReader = sbmlOdeSolver.SBMLReader_create()
-    doc = sbmlOdeSolver.SBMLReader_readSBML(sbmlReader, filename)
-    return doc
-
-def settingIntegrationParameter(time, printstep):
-    """Set the integration parameters and the integration time"""
-    settings = sbmlOdeSolver.CvodeSettings_create()
-    print time, printstep
-    sbmlOdeSolver.CvodeSettings_setTime(settings, CT.c_double(time), CT.c_int(printstep) )
-    absoluteErrorTollerance = CT.c_double(1e-9)
-    relativeErrorTollerance = CT.c_double(1e-4)
-    mxStep = CT.c_int(1000)
-    sbmlOdeSolver.CvodeSettings_setErrors(settings, absoluteErrorTollerance, 
-                                          relativeErrorTollerance, mxStep)
-    sbmlOdeSolver.CvodeSettings_setCompileFunctions(settings, CT.c_int(1))
-    sbmlOdeSolver.CvodeSettings_dump(settings)
-    return settings
-    
-  #calling the SBML ODE Solver which returns SBMLResults
-def getResults(doc, settings):
-    """get all the results for the integration"""
-    results = sbmlOdeSolver.SBML_odeSolver(doc, settings)
-    
-    if results ==  False:
-        print "Integration not sucessful!\n"
-        print sbmlOdeSolver.SolverError_dumpAndClearErrors()
-        #return sbmlOdeSolver.EXIT_FAILURE
-    else:
-        sbmlOdeSolver.SolverError_dumpAndClearErrors()
-        return results
-    
-def retrieveTimecourses(results, sbmlId):
-    """Return the time and the values of the sbml Variable.
-    :Parameter
-        results: the integration results
-        sbmlId: the sbml variable to retrieve
-    :Return
-        timecourse: numpy array with: 
-            first column: the time points
-            second column: the values"""
+    def __init__(self):
+        # in theory this whole thing should be platform independent
+        # note: Change the name of the library
+        if os.sys.platform == 'win32':
+            print 'Opening windows.'
+            self.soslib = CT.CDLL('c://windows//system32//libODES.dll')
+        # Also available for MacOS although I don't know all the details.
+        # if you are using a Mac please check the results of 'os.sys.platform'
+        # and set your absolute path to libsbml3 and let me know if it works
+        elif os.sys.platform == 'darwin' or os.name == 'mac':
+            print 'Here be apples.'
+            self.soslib = CT.CDLL('/usr/lib/libODES.dylib', CT.RTLD_GLOBAL)
+        else:
+            print 'Here be penguins.'
+            self.soslib = CT.CDLL('/usr/local/lib/libODES.so')
+        self.setReturnType()
+            
         
-    timeCourse = sbmlOdeSolver.SBMLResults_getTimeCourse(results, sbmlId)
-    timePoints = sbmlOdeSolver.TimeCourse_getNumValues(timeCourse)
-    #print "number of timepoints: %d" %timePoints
+        ########################################################
     
-    values = []
-
-    for i in range(timePoints):
-        values.append(sbmlOdeSolver.TimeCourse_getValue(timeCourse, CT.c_int(i)))
-    time = numpy.arange(timePoints)
-    
-    val = numpy.array(values)
-    return numpy.column_stack((time, val)) # timecourse
-    
-def integrate(filename, time, printstep, variables):
-    """Integrate a model"""
-    setReturnType()
-    doc = loadModel(filename)
-    settings = settingIntegrationParameter(time, printstep)
-    results = getResults(doc, settings)
-    plotTimeCourses(results, variables)
- 
-    return results
-
-def plotTimeCourses(results, variables):
-       # Getting the timeCourse
-    tcDict = {}
-    for var in variables:
-        tcDict[var] = retrieveTimecourses(results, var)
+    def setReturnType(self): 
+        """Set the return type in an appropriate way"""
         
-    for var, tc in tcDict.iteritems():
-        pylab.plot(tc[:,0], tc[:,1], label=var)
-
-    pylab.legend(loc=0)
-    pylab.show()
+        self.soslib.TimeCourse_getName.restype = CT.c_char_p # Return a string
+        self.soslib.TimeCourse_getValue.restype = CT.c_double # Return a double
+        self.soslib.IntegratorInstance_getVariableValue.restype = CT.c_double
+        self.soslib.IntegratorInstance_getTime.restype = CT.c_double
     
     
-def changeValue(odemodel, settings, var_idx):
-    print "Creating the integrator instance"
-    integratorInstance = sbmlOdeSolver.IntegratorInstance_create(odeModel, settings)
     
-    vars = var_idx.keys()
-    vars_val = {}
-    for var in vars:
-        vars_val[var] = []
-    printstep = sbmlOdeSolver.CvodeSettings_getPrintsteps(settings)
-    #caInput = calciumTrain(60000)
-    for i in range (printstep):
         
-        sbmlOdeSolver.IntegratorInstance_integrateOneStep(integratorInstance)
+    def retrieveTimecourses(self, results, sbmlId):
+        """Return the time and the values of the sbml Variable.
+        :Parameter
+            results: the integration results
+            sbmlId: the sbml variable to retrieve
+        :Return
+            timecourse: numpy array with: 
+                first column: the time points
+                second column: the values"""
+            
+        timeCourse = self.soslib.SBMLResults_getTimeCourse(results, sbmlId)
+        timePoints = self.soslib.TimeCourse_getNumValues(timeCourse)
+        #print "number of timepoints: %d" %timePoints
+        
+        values = []
+    
+        for i in range(timePoints):
+            values.append(self.soslib.TimeCourse_getValue(timeCourse, CT.c_int(i)))
+        time = numpy.arange(timePoints)
+        
+        val = numpy.array(values)
+        return numpy.column_stack((time, val)) # timecourse
         
         
-    #print s1_value, sbmlOdeSolver.IntegratorInstance_getTime(integratorInstance)
-#        if i == 55000:
- #           sbmlOdeSolver.IntegratorInstance_setVariableValue(integratorInstance, var_idx["species_1"], CT.c_double(0.008))
-#        if i in caInput:
-#            sbmlOdeSolver.IntegratorInstance_setVariableValue(integratorInstance, var_idx["species_1"], CT.c_double(0.08))
-#            print "input Calcium at time: %d" %i
-#            
-        for var in var_idx:
-            var_value = sbmlOdeSolver.IntegratorInstance_getVariableValue(integratorInstance, 
-                                                                          var_idx[var])
-            vars_val[var].append(var_value)
-
+    def changeValue(self, odemodel, settings, var_idx):
+        print "Creating the integrator instance"
+        integratorInstance = self.soslib.IntegratorInstance_create(odeModel, settings)
         
-    print "Out of the for loop"
-    sbmlOdeSolver.IntegratorInstance_dumpSolver(integratorInstance)
-    return vars_val
-
-def plotVars(vars_val, printstep):
-    time = numpy.arange(printstep)
-    for var in vars_val:
-        pylab.figure()
-        var_tc = numpy.array(vars_val[var])
-        pylab.plot(time, var_tc, label=id2name[var])
+        vars = var_idx.keys()
+        vars_val = {}
+        for var in vars:
+            vars_val[var] = []
+        printstep = self.soslib.CvodeSettings_getPrintsteps(settings)
+        #caInput = calciumTrain(6000)
+        
+        print "integration running till: %d" %printstep
+        #print caInput
+        for i in range (printstep):
+            
+            self.soslib.IntegratorInstance_integrateOneStep(integratorInstance)
+            
+            #Calcium conc coming from the conc.
+            #self.soslib.IntegratorInstance_setVariableValue( integratorInstance, var_idx["species_1"], 
+            #calciumDynamics(i, caInput))
+            
+        #print s1_value, self.soslib.IntegratorInstance_getTime(integratorInstance)
+    #        if i == 5000:
+    #           self.soslib.IntegratorInstance_setVariableValue(integratorInstance, var_idx["species_1"], CT.c_double(0.008))
+           # if i in caInput:
+             #   self.soslib.IntegratorInstance_setVariableValue(integratorInstance, var_idx["species_1"], CT.c_double(0.08))
+            #    print "input Calcium at time: %d" %i
+    #            
+            for var in var_idx:
+                var_value = self.soslib.IntegratorInstance_getVariableValue(integratorInstance, 
+                                                                              var_idx[var])
+                vars_val[var].append(var_value)
+    
+            
+        print "Out of the for loop"
+        self.soslib.IntegratorInstance_dumpSolver(integratorInstance)
+        return vars_val
+    
+    def plotVars(self, vars_val, endTime, resolution):
+        """Plot the results."""
+        printstep = endTime * resolution
+        time = numpy.arange(printstep)
+        time_in_sec = time/ float(resolution)
+        # The Bars    
+        bars = ['PP2Bbar', 'CaMKIIbar']
+        for bar in bars:
+            var_tc = numpy.array(vars_val[name2id[bar]])
+            pylab.plot(time_in_sec, var_tc, label=bar)
+        pylab.title("Activation of PP2B and CamKII")
+        pylab.xlabel("Time [s]")
         pylab.legend(loc=0)
-    pylab.show()
-    
-def calciumTrain(start, spikes=30, interval=100):
-    caInput = [start]
-    for spike in range(spikes):
-        caInput.append(caInput[-1] + interval)
-    return caInput
-
-def calciumDynamics(time, start):
-    basal = 0.00006 # What is the basal value?
-    if time > start: 
         
-#        NO RIPENSA L'ALGORITMO!
-#    il calcio deve essere scrito solo se  nel picco,
-#    altrimenti dai il basal.
+        # Calcium input
+        pylab.figure()
+        var_tc = numpy.array(vars_val[name2id['Ca']])
+        pylab.plot(time_in_sec, var_tc, label='Ca')
+        pylab.title("Calcium input")
+        pylab.xlabel("Time [s]")
+        pylab.legend(loc=0)
+        
+        pylab.show()
+        
+    def calciumTrain(self, start, spikes=30, interval=100):
+        caInput = [start]
+        for spike in range(spikes):
+            caInput.append(caInput[-1] + interval)
+        return caInput
     
-        ca = calcium_peak(7200, time)
-
-def expDecay(ca0, time, k = 4.0e8):
-    """Simple exponential decay
-    k coming from Lu model"""
-    y = ca0 * k  * exp(-time)
-    return y
-
-def calcium_peak(ca0, time):
-    """
-    7200 molecules have to go in.
-    Simple exponential decay
-    k coming from Lu model"""
+    def calciumDynamics(self, time, caInput):
+        basal = 0.00006 # What is the basal value?
+        if len(caInput) == 1: # Last term
+            ca = self.calciumPeak(7200, time)
+            print ca, time, basal
+            caInput.pop(0)
+            if ca > basal:
+                return ca
+            else: 
+                return basal
+        elif time >= caInput[0] and time < caInput[1] :
+            ca = self.calcium_peak(7200, time)
+            caInput.pop(0)
+            if ca > basal:
+                return ca
+            else: 
+                return basal
+        else:
+            return basal
+            
     
-    ca = ca0 * k  * exp(-time)
-    return ca
-    
-    #basal = ca_in['k']
-    print "basal calcium: %d" %basal
-#    sbmlOdeSolver.IntegratorInstance_setVariableValue(integratorInstance, 
-#                                                      var_idx[name2id['Ca']], 
-#                                                      CT.c_double(expDecay(calConc(7200), time) ))
-    
-    
-def setSettings(endtTime):
-    settings = sbmlOdeSolver.CvodeSettings_create()
-    
-    #printstep = endTime * 1000 #ms
-    printstep = endtTime
-    sbmlOdeSolver.CvodeSettings_setTime(settings, CT.c_double(endTime), 
-                                        CT.c_int(printstep))
-    
-    absoluteErrorTollerance = CT.c_double(1e-9)
-    relativeErrorTollerance = CT.c_double(1e-4)
-    mxStep = CT.c_int(1000)
-    sbmlOdeSolver.CvodeSettings_setErrors(settings, absoluteErrorTollerance, 
-                                          relativeErrorTollerance, mxStep)
-    sbmlOdeSolver.CvodeSettings_setSwitches(settings, 1, 0, 0, 0, 1, 0, 0)
-    return settings    
+    def calciumPeak(self, ca0, time, k = 4.0e8):
+        """
+        7200 molecules have to go in.
+        Simple exponential decay
+        k coming from Lu model"""
+        
+        ca = ca0 * k  * exp(-time)
+        return ca
+        
+    def setSettings(self, endtTime, resolution):
+        settings = self.soslib.CvodeSettings_create()
+        
+        printstep = endTime * resolution
+        self.soslib.CvodeSettings_setTime(settings, CT.c_double(endTime), 
+                                            CT.c_int(printstep))
+        
+        absoluteErrorTollerance = CT.c_double(1e-9)
+        relativeErrorTollerance = CT.c_double(1e-4)
+        mxStep = CT.c_int(1000)
+        self.soslib.CvodeSettings_setErrors(settings, absoluteErrorTollerance, 
+                                              relativeErrorTollerance, mxStep)
+        self.soslib.CvodeSettings_setSwitches(settings, 1, 0, 0, 0, 1, 0, 0)
+        return settings    
     
 if __name__ == "__main__":
-    setReturnType()
+    sosWrap = SOSWrap()
     id2name = {"parameter_28" : "CaMKIIbar", 
                "parameter_34" : "PP2Bbar",
                "species_1" : "Ca"
+                              }
+    name2id = {"CaMKIIbar" : "parameter_28", 
+               "PP2Bbar" : "parameter_34",
+               "Ca" : "species_1"
                               }
 
 
@@ -223,15 +197,16 @@ if __name__ == "__main__":
     #filename="../biochemical_circuits/testSimple.xml"
     #variables = ["s1","s3"]
 
-    odeModel = sbmlOdeSolver.ODEModel_createFromFile(filename)
+    odeModel = sosWrap.soslib.ODEModel_createFromFile(filename)
 
     var_idx = {}
     for var in variables:
-        var_idx[var] = sbmlOdeSolver.ODEModel_getVariableIndex(odeModel, var)
+        var_idx[var] = sosWrap.soslib.ODEModel_getVariableIndex(odeModel, var)
     
-    endTime = 12000
-    settings = setSettings(endTime)
-    vars_val = changeValue(odeModel, settings, var_idx)
-    printstep = sbmlOdeSolver.CvodeSettings_getPrintsteps(settings)
-    plotVars(vars_val, printstep)
+    endTime = 10    
+    resolution = 1000 # This determine the timesteps of the model. 1000 = ms
+    settings = sosWrap.setSettings(endTime, resolution)
+    vars_val = sosWrap.changeValue(odeModel, settings, var_idx)
+    printstep = sosWrap.soslib.CvodeSettings_getPrintsteps(settings)
+    sosWrap.plotVars(vars_val, endTime, resolution)
 
