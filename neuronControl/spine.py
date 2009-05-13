50ef881b469e4e047901c58b647a7b9fd4b53dc3
Michele Mattioni <mattioni@ebi.ac.uk>
2009-05-13 10:57:54 +0100

    Synapses created at the init of the spine.
    No stimul is applied (all the parameter are 0)
    Everything is ready to be changed later

--- neuronControl/spine.py
+++ neuronControl/spine.py
@@ -5,6 +5,7 @@ from neuron import h, nrn
 import ecellControl as eC
 import math
 import os
+from synapse import Synapse
 
 
 class mySection(nrn.Section):
@@ -25,16 +26,11 @@ class Spine():
         self.head = self.createHead(self.neck)
         self.psd = self.createPSD(self.head)
         self.parent = None # the parent section connected to the neck
-        self.synapses = {} # Dict to save the pointer to the synapses
+        self.synapses = self.createSynapses() # Dict to save the pointer to the synapses
         
         # Setting up the biochemical simulator
         self.gcw = os.getcwd()
         self.ecellMan = self.setupBioSim(filename_bioch_mod)
-        
-    def updateName(self):
-        """Update the name of the sections. Call this method only when you will not change the section"""
-        self.neck.rename()
-        self.head.rename()
         
     def setupBioSim(self, filename):
         """Initialize the Biochemical Simulator creating the instance of 
@@ -107,24 +103,27 @@ class Spine():
         
         return psd
         
+    def createSynapses(self):
+        "Create an AMPA and an NMDA synapse in the spine"
+        synapses = {}
+        # AMPA Syn
         
+        ampaSyn = Synapse('ampa', self.psd)
+        ampaSyn.createStimul()
+        synapses['ampa'] = ampaSyn
+        
+        #NMDA Syn
+        nmdaSyn = Synapse('nmda', self.psd)
+        nmdaSyn.createStimul()
+        synapses['nmda'] = nmdaSyn
+        
+        return synapses    
     
     def attach(self, parentSec, parentx, childx):
         """Attach a spine to a parentSec and store the parentSec into an attribute.
         Just an handy variation of the connect method"""
         self.neck.connect(parentSec, parentx, childx)
         self.parent = parentSec
-        
-    def addSynapse(self, synapseName, synapseObj):
-        """Add a synapese to the internal dictionary"""
-        self.synapses[synapseName] = synapseObj
-    
-    def getSynapse(self, synapseName):
-        """Return the synapse object"""
-        if synapseName in self.synapses.keys():
-            return self.synapses[synapseName]
-        else:
-            return None
 
         
 if __name__ == "__main__":
@@ -140,6 +139,7 @@ if __name__ == "__main__":
     mod_path = "../mod"
     hoc_path = "../hoc"
         
+        # Mod file are always in a mod directory
     if not os.path.exists(mod_path) :
         print "ERROR mod path %s doesn't exist" %os.path.realpath(mod_path)
         sys.exit(1)
@@ -148,28 +148,29 @@ if __name__ == "__main__":
     if not os.path.exists(hoc_path):
         print "ERROR hoc path %s doesn't exist" %os.path.realpath(hoc_path)
         sys.exit(1)
+    # Hoc file assumes all the file are launched from a top directory
+    head, tail  = os.path.split(os.path.realpath(hoc_path))
+
+    preface_pos = head
     
-    os.chdir(hoc_path)
-    preface_pos = os.getcwd()
-        
     h('strdef preface, dirstr') # preface and dirstr used in each hoc
     preface_string = "preface = \"" + preface_pos + "\""
     h(preface_string)
-    h.load_file( "all_tau_vecs.hoc")
+    h.load_file(os.path.join(hoc_path, "all_tau_vecs.hoc"))
         
     print "Testing the spine. Current directory %s" %os.getcwd()
     spine1 = Spine("spine1", 
                    filename_bioch_mod ="../biochemical_circuits/biomd183_noCalcium.eml")
     
     # AMPA Syn
-    ampaSyn = Synapse('ampa', spine1.psd)
-    ampaSyn.createStimul(start=130, number=10, interval=10, noise=0)
-    spine1.addSynapse("ampa", ampaSyn)
+    ampaSyn = spine1.synapses['ampa']
+    ampaSyn.netStim.start = 200
+    ampaSyn.netStim.number = 1
+    ampaSyn.netStim.interval = 5
+    ampaSyn.netStim.noise = 0
     
     #NMDA Syn
-    nmdaSyn = Synapse('nmda', spine1.psd)
-    nmdaSyn.createStimul(start=130, number=10, interval=10, noise=0)
-    spine1.addSynapse("nmda", nmdaSyn)
+    
 
     # Plotting stuff
     
@@ -186,10 +187,10 @@ if __name__ == "__main__":
     h.v_init = -87.75 # Setting the initial vm
     h.dt = 0.005
     
-    h.tstop = 500
+    h.tstop = 400
     h.run()
     graph.plotCalcium(vecs, "cai")
     graph.plotCalcium(vecs, "cali")
-    graph.plotVoltage(vecsVolt, spine1.getSynapse('ampa').synVecs)
-    graph.plotVoltage(vecsVolt, spine1.getSynapse('nmda').synVecs)
+    graph.plotVoltage(vecsVolt, spine1.synapses['ampa'].synVecs)
+    graph.plotVoltage(vecsVolt, spine1.synapses['nmda'].synVecs)
     pylab.show()
