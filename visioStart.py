cb031b01d4e7a5ca4a7f7af2d570604612f3942f
Michele Mattioni <mattioni@ebi.ac.uk>
2009-07-02 21:14:21 +0100

    Added the script to start neuronVisio

---
+++ visioStart.py
@@ -0,0 +1,6 @@
+import nrnVisio
+from neuron import h
+import neuronControl
+nrnSim = neuronControl.NeuronSim(mod_path="mod", hoc_path="hoc", 
+                              spines=True, biochemical=False, 
+                              biochemical_filename="biochemical_circuits/biomd183_noCalcium.eml")
