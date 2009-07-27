0a427b3a9e321c8625a216764554f89fc8403752
Michele Mattioni <mattioni@ebi.ac.uk>
2009-07-27 10:21:58 +0100

    Modified the starting script for neuronVisio with the last API

--- visioStart.py
+++ visioStart.py
@@ -1,6 +1,8 @@
 import nrnVisio
 from neuron import h
+controls = nrnVisio.Controls()
 import neuronControl
 nrnSim = neuronControl.NeuronSim(mod_path="mod", hoc_path="hoc", 
                               spines=True, biochemical=False, 
                               biochemical_filename="biochemical_circuits/biomd183_noCalcium.eml")
+                              
