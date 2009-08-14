ee15e09d5287245212e04dbe79b12560120f7d27
Michele Mattioni <mattioni@ebi.ac.uk>
2009-08-14 08:45:54 +0100

    Using one spine only for testing

--- neuronControl/nrnSim.py
+++ neuronControl/nrnSim.py
@@ -79,20 +79,20 @@ class NeuronSim():
         self.spines = []
         
         # Trying one spine only for test
-        spine_positions = [0.5]
-        self.__populateDend(spine_positions, [h.MSP_Cell[0].dend3_1[1]])
+#        spine_positions = [0.5]
+#        self.__populateDend(spine_positions, [h.MSP_Cell[0].dend3_1[1]])
 
         
         # Distal:
-        #spine_positions = [0.1, 0.21, 0.23, 0.25, 0.27, 0.29, 0.30, 0.50, 0.7]
-        #spine_positions = [0.1, 0.21, 0.25, 0.30, 0.50, 0.7]
-        #self.__populateDend(spine_positions, h.MSP_Cell[0].Dist_Dend)
+        spine_positions = [0.1, 0.21, 0.23, 0.25, 0.27, 0.29, 0.30, 0.50, 0.7]
+        spine_positions = [0.1, 0.21, 0.25, 0.30, 0.50, 0.7]
+        self.__populateDend(spine_positions, h.MSP_Cell[0].Dist_Dend)
         
         
         # Mid
-#        print "Adding the Mid spines"
-#        spine_positions = [0.3, 0.7, 0.9]
-#        self.__populateDend(spine_positions, h.MSP_Cell[0].Mid_Dend)
+        print "Adding the Mid spines"
+        spine_positions = [0.3, 0.7, 0.9]
+        self.__populateDend(spine_positions, h.MSP_Cell[0].Mid_Dend)
     
     def __populateDend(self, spine_positions, dendList):
         """Distribuete the psines among the dends"""
