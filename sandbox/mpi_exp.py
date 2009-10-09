dcf154c7e261da164c96c1bf20f8db8ebadd6141
Michele Mattioni <mattioni@ebi.ac.uk>
2009-10-09 15:13:48 +0100

    changing the order of the import

--- sandbox/mpi_exp.py
+++ sandbox/mpi_exp.py
@@ -1,11 +1,12 @@
+from mpi4py import MPI
 import os
 
 # Removing display to import neuron properly
 if os.environ.has_key("DISPLAY"):
     del os.environ['DISPLAY']
 
-from mpi4py import MPI
 from neuron import h
+
 pc = h.ParallelContext()
 
 s = "mpi4py thinks I am %d of %d, NEURON thinks I am %d of %d\n"
