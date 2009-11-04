733961e1e24c8f717d74a7e2c6711a6eaea285ec
Michele Mattioni <mattioni@ebi.ac.uk>
2009-11-04 16:28:38 +0000

    Added the label on the axes in the importing script

--- sandbox/import_digitezed_data.py
+++ sandbox/import_digitezed_data.py
@@ -6,5 +6,9 @@ import pylab
 
 a = pylab.csv2rec('spines_distribution_Wilson_1992.csv')
 
-plot(a.x, a.y)
+pylab.plot(a.x, a.y)
+pylab.xlabel("Distance from the soma [um]")
+pylab.ylabel("Surface Area [um]/Dendritic Lenght [um^2]")
+
+pylab.show()
 
