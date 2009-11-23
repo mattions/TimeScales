4c53f0352f5c8a6f5d678ad2fcf602311903d613
Michele Mattioni <mattioni@ebi.ac.uk>
2009-11-23 15:28:00 +0000

    rewritten the core code of the math to a single c function not visible from python.

--- mathhelper.pyx
+++ mathhelper.pyx
@@ -3,13 +3,17 @@
 
 cdef extern from "math.h":
     double pow(double, double)
+
+cdef calc(double CaMKIIbar, int n, int k):
+    """Calc the math function down to the C for performance reason"""
+    return pow(CaMKIIbar, n) / (pow(k, n) + pow(CaMKIIbar, n))
     
 cpdef calcWeight(double old_weight, double CaMKIIbar, int n, int k):
     """Calc the weight of the synapses according to the CaMKII"""
     
     # Dummy function should be changed
     cdef double delta, weight
-    delta = pow(CaMKIIbar, n) / (pow(k, n) + pow(CaMKIIbar, n))
+    delta = calc(CaMKIIbar, n, k)
     weight = old_weight + delta
 #    s = "Old weight %f, CAMKIIbar value: %e,\
 #    calculated delta: %e" %(old_weight, CaMKIIbar, delta)
