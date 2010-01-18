2dee2dd82a6c169aed7950839d1808e0862d0f1d
Michele Mattioni <mattioni@ebi.ac.uk>
2010-01-18 11:43:54 +0000

    Using cylinders with a more complicated coordinates to test.

--- sandbox/mayavi_visual.py
+++ sandbox/mayavi_visual.py
@@ -1,13 +1,14 @@
 from enthought.mayavi import mlab
 from enthought.tvtk.tools import visual
+
 # Create a figure
 f = mlab.figure(size=(500,500))
 # Tell visual to use this as the viewer.
 visual.set_viewer(f)
 
-# Even sillier animation.
-cyl = visual.Cylinder(pos=(0.,0.,0.), radius=8, length=10)
-cyl2 = visual.Cylinder(pos=(8.,0.,0.,), axis=(0.,4.,5.), radius=4, length=13)
-cyl3 = visual.Cylinder(pos=(-4.,0.,0.))
-
-
+cyl = visual.Cylinder(pos=(0.,0.,0.,), axis=(100.,0.,0.), 
+                       radius=5, length=100.0)
+cyl2 = visual.Cylinder(pos=(100.,0.,0,), axis=(92.1060943604,38.9418334961,0), 
+                       radius=1.5, length=100.0)
+cyl3 = visual.Cylinder(pos=(100.0,0.,0.), axis=(92.1060943604,-38.9418334961,0), 
+                       radius=1.5, length=100.0)
