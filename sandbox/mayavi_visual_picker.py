967c8f0c471eb52e1c8e519ef1c864eb4b6e89c2
Michele Mattioni <mattioni@ebi.ac.uk>
2010-02-09 12:02:02 +0000

    Testing the picking for the mayavi scene

--- sandbox/mayavi_visual_picker.py
+++ sandbox/mayavi_visual_picker.py
@@ -15,13 +15,16 @@ cyl3 = visual.Cylinder(pos=(100.0,0.,0.), axis=(92.1060943604,-38.9418334961,0),
                        
 cyls = [cyl, cyl2, cyl3]
 
-def picker_callback(picker_obj):
+#sphere = visual.Sphere()
+
+def picker_callback(picker):
     """ Picker callback: this get called when on pick events. 
     """
-    picked = picker_obj.actor
-    print picked
-    if picked is not None:
-        print "Picked something"
-
+    picked = picker.actor
+    print picked.__class__,
+    for cyl in cyls:
+        if picked == cyl.actor:
+            print "Picked something"
+            cyl.color = (1.,0.,0.)
 
 picker = f.on_mouse_pick(picker_callback)
