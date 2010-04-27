2b2d1dea544d9df63a74ab488949d2049c5856e5
Michele Mattioni <mattioni@ebi.ac.uk>
2010-04-27 18:24:55 +0100

    move to zero spine for testing purpose

--- visioStart.py
+++ visioStart.py
@@ -3,6 +3,6 @@ controls = Controls()
 from neuron import h
 import neuronControl
 nrnSim = neuronControl.NeuronSim(mod_path="mod", hoc_path="hoc", 
-                              spines_dist='two', 
+                              spines_dist='zero', 
                               biochemical_filename="biochemical_circuits/biomd183_noCalcium.eml")
                               
