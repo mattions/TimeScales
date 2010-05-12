c0d6a67314ea7d666cc8512013d75ebe0daf39b9
Michele Mattioni <mattioni@ebi.ac.uk>
2010-05-12 12:18:16 +0100

    Changed the visio to all spines for testing purpose

--- visioStart.py
+++ visioStart.py
@@ -3,6 +3,6 @@ controls = Controls()
 from neuron import h
 import neuronControl
 nrnSim = neuronControl.NeuronSim(mod_path="mod", hoc_path="hoc", 
-                              spines_dist='zero', 
+                              spines_dist='all', 
                               biochemical_filename="biochemical_circuits/biomd183_noCalcium.eml")
                               
