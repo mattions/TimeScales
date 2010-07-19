ce9e65c362b21cd702b32a337f4fdbc02ac25966
Michele Mattioni <mattioni@ebi.ac.uk>
2010-07-19 16:16:37 +0100

    Commenting the multithread exp.

--- visioStart.py
+++ visioStart.py
@@ -2,7 +2,35 @@ from neuronvisio.controls import Controls
 controls = Controls()
 from neuron import h
 import neuronControl
+import time
+
+
 nrnSim = neuronControl.NeuronSim(mod_path="mod", hoc_path="hoc", 
                               spines_dist='all', 
                               biochemical_filename="biochemical_circuits/biomd183_noCalcium.eml")
-                              
+
+#start= time.time()
+#                  
+#controls.manager.add_all_vecRef('v') # Adding vector for the variable v
+#
+#print "Starting time: %s" % start            
+#h.load_file('parcom.hoc')
+#p = h.ParallelComputeTool()
+#p.nthread(4)
+#p.multisplit(True)
+#
+#h.finitialize()
+#while h.t < h.tstop:
+#    h.fadvance()
+#
+#end= time.time()
+#
+#run_time = end-start
+#
+#print "Ending time: %s. Total time with no parallel: %s" %(end, run_time)
+#print "Tstop: %s" %(h.tstop)
+#
+## file where to save the results
+#filename = 'storage.h5'
+## Saving the vectors
+#controls.manager.save_to_hdf(filename)
