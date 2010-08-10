11193c17a3fc5bc8e2c753d2ede6deb6861ba54d
Michele Mattioni <mattioni@ebi.ac.uk>
2010-08-10 20:44:06 +0100

    Adding the extref module to hold all my extref.

---
+++ extref.py
@@ -0,0 +1,86 @@
+# Michele Mattioni
+# Tue Aug 10 09:08:06 BST 2010
+
+from neuronvisio.manager import BaseRef
+
+class TimeSeries(BaseRef):
+    """
+    Extend the classic VecRef from Neuronvisio to allocate 
+    the biochemical results.
+    """
+    
+    def __init__(self, sec_name=None, vecs=None, detail=None):
+        
+        BaseRef.__init__(self)
+        self.sec_name = sec_name
+        self.vecs = vecs
+        self.detail = detail
+
+class WeightRef(BaseRef):
+    """
+    Extend the classic VecRef from Neuronvisio to allocate 
+    the biochemical results.
+    """
+    
+    def __init__(self, sec_name=None, vecs=None, detail=None):
+        
+        BaseRef.__init__(self)
+        self.sec_name = sec_name
+        self.vecs = vecs
+        self.detail = detail
+
+
+
+class ExtRef(object):
+    "Holds all the methods to add new ref to the manager"
+    
+
+    def add_timeseries(self, manager, stim_spines, nrnSim):
+        """Adding timeseries from the ecell world to the manager"""
+        for spine_id in stim_spines:
+            
+            spine = nrnSim.spines[spine_id]
+            # Retrieving the biochemical timecourses
+            spine.ecellMan.converToTimeCourses()
+            time_courses = spine.ecellMan.timeCourses 
+            
+            pos = str(spine.pos)
+            parent = spine.parent.name()
+            detail = parent + "_" + pos
+            sec_name = str(spine.id)
+            
+            # Adding a record for each variable
+            vecs = {}
+            time = None
+            for var in time_courses.keys():
+                time = time_courses[var][:,0]
+                vecs[var] = time_courses[var][:,1]
+                
+            timeseriesRef = TimeSeries(sec_name=sec_name, 
+                                       vecs=vecs,
+                                       detail=detail)
+            manager.add_ref(timeseriesRef, time)
+            
+    def add_weights(self, manager, stim_spines, nrnSim):
+        """Add the weight to the manager"""
+        for spine_id in stim_spines:
+            spine = nrnSim.spines[spine_id]
+            
+            pos = str(spine.pos)
+            parent = spine.parent.name()
+            detail = parent + "_" + pos
+            sec_name = str(spine.id)
+            
+            vecs = {}
+            time = None
+            
+            for syn in spine.synapses:
+                if syn.chan_type == 'ampa':
+                    if syn.weight[0]:
+                        time = syn.weight[0]
+                        weight = syn.weight[1]
+                        vecs['weight'] = weight
+                        weightRef = WeightRef(sec_name=sec_name,
+                                              vecs=vecs,
+                                              detail=detail)
+                        manager.add_ref(weightRef, time)
