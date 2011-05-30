aedb13031eb7d2659acc869460008b7098049b8e
Michele Mattioni <mattioni@ebi.ac.uk>
2011-05-30 11:34:59 +0100

    Reloading NEuron for the plotter, so we overcome the global variable problem, and fix the limits for the voltage.

--- sandbox/plotter_walker.py
+++ sandbox/plotter_walker.py
@@ -1,11 +1,8 @@
-from helpers.plotter import DoublePlotter
-
-from neuronvisio.manager import Manager
-
 import matplotlib.pyplot as plt
 import os 
 
-
+from helpers.plotter import DoublePlotter
+import neuronvisio.manager
 
 filename = 'storage.h5'
 
@@ -22,16 +19,20 @@ dirs = {'1spine_8Hz' : 'Data_rmt/05-05-2011/Sim_0/',
 
 for condition, dir in dirs.iteritems():
     h5_filename = dir + 'storage.h5'
-    man = Manager()
+    neuronvisio.manager = reload(neuronvisio.manager)
+    man = neuronvisio.manager.Manager()
     man.load_from_hdf(h5_filename)
     dp = DoublePlotter()
     
     biogroup = 'timeSeries_' + spine_num
+    fig = plt.figure()
     ax1, ax2 = dp.plot_double_axes(man, spine_num, 'v', 
         'AMPAR_P', bio_group=biogroup)
+    ax1.set_ylim(-90, -50)
     
     for ext in ['.png', '.pdf']:
         name = 'ele_bio' + condition + ext
         fig_filename = os.path.join(dir, name)
         plt.savefig(fig_filename)
         print "Saved file %s" %fig_filename
+    del man
