8faa912c0094706268c2be6c1b6bd046515913e4
Michele Mattioni <mattioni@ebi.ac.uk>
2011-06-09 16:26:32 +0100

    Added the other dirs to the plot_walker.

--- helpers/plotter_walker.py
+++ helpers/plotter_walker.py
@@ -6,15 +6,18 @@ import neuronvisio.manager
 
 filename = 'storage.h5'
 
-#stimulated_spines = ['spine554', 'spine555', 'spine556', 
-#                     'spine558', 'spine559', 'spine560', 
-#                     'spine562', 'spine563', 'spine564', 
-#                     'spine1468', 'spine1469', 'spine1470', 
-#                     'spine1472', 'spine1473', 'spine1474', 
-#                     'spine1476', 'spine1477', 'spine1478']
+stimulated_spines = ['spine554', 'spine555', 'spine556', 
+                     'spine558', 'spine559', 'spine560', 
+                     'spine562', 'spine563', 'spine564', 
+                     'spine1468', 'spine1469', 'spine1470', 
+                     'spine1472', 'spine1473', 'spine1474', 
+                     'spine1476', 'spine1477', 'spine1478']
 
-stimulated_spines = ['spine554']
-dirs = {'cpm_8Hz' : 'Data_rmt/30-04-2011/Sim_0/',
+#stimulated_spines = ['spine554']
+dirs = {#'cpm_8Hz' : 'Data_rmt/30-04-2011/Sim_0/',
+        'cpm_20Hz' : 'Data_rmt/29-04-2011/Sim_2/',
+        'cpm_40Hz' : 'Data_rmt/29-04-2011/Sim_1/',
+        'cmp_50Hz' : 'Data_rmt/29-04-2011/Sim_0'
         }
 
 
@@ -38,7 +41,7 @@ for condition, dir in dirs.iteritems():
         sp.plot_input(spine_num, man, ax=ax1)
         
         for ext in ['.png', '.pdf']:
-            name = 'ele_bio' + condition + ext
+            name = 'ele_bio_' + condition + "_" + spine_num + ext
             fig_filename = os.path.join(dir, name)
             plt.savefig(fig_filename)
             print "Saved file %s" %fig_filename
