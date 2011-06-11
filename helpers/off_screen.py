6f16eb23a7d17f36aef6b8db0327adad7fde3c8b
Michele Mattioni <mattioni@ebi.ac.uk>
2011-06-11 09:46:26 +0100

    introduced the index instead of the time point.

--- helpers/off_screen.py
+++ helpers/off_screen.py
@@ -3,7 +3,7 @@ from shutil import copyfile
 
 from enthought.mayavi import mlab
 # Offscreen
-#mlab.options.offscreen = True
+mlab.options.offscreen = True
 
 import neuronvisio.controls
 from neuronvisio.visio import Visio
@@ -30,10 +30,11 @@ visio.draw_model()
 time = man.groups['t']
 for i, time_point in enumerate(time):
 
-    visio.show_variable_timecourse(var, time_point, 
+    
+    visio.show_variable_timecourse(var, i, 
                                  start_value, end_value)
-    #visio.mayavi.visualization.scene.render()
     #visio.timelabel.text = str(round(time_point, 3))
+    
     figure_filename_screenshot = 'medium_model_%s_%09d.png' %(time_point, i)
     figure_filename_animation = 'medium_model%09d.png' %(i)
     
@@ -42,10 +43,8 @@ for i, time_point in enumerate(time):
         os.mkdir(animation_dir)
     if not os.path.exists(screenshot_dir):
         os.mkdir(screenshot_dir)
-        
+    
     print "Saving %s" %figure_filename_screenshot
-    # Force the redraw
-    #mlab.draw()
     mlab.savefig(os.path.join(animation_dir, figure_filename_animation), 
                  size=(1280, 1024))
     copyfile(os.path.join(animation_dir, figure_filename_animation), 
