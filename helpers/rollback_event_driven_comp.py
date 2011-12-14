7d4a273f26c838e43c052d8c47f173e10b77dc6d
Michele Mattioni <mattioni@ebi.ac.uk>
2011-12-14 10:43:46 +0000

    inserted the value for the latest run from the cluster.

--- helpers/rollback_event_driven_comp.py
+++ helpers/rollback_event_driven_comp.py
@@ -11,9 +11,23 @@ x = Num_inputs * FIRST_TRAIN_STIM + Num_inputs * SECOND_TRAIN_STIM
 
 # Time Analysis
 
-y_roll = [62678.12, 102494.94, 109417.59, 136696.58] #s
+
+r_10 = array([62171.03, 73263.17, 75811.31])
+r_20 = array([81626.53, 74990.55, 72281.77])
+r_30 = array([71874.88 , 83235.84, 78298.66, 75257.20])
+r_40 = array([83678.98, 78842.45, 85668.27 ])
+
+e_10 = array([70542.38])
+e_20 = array([75348.20, 77029.46])
+e_30 = array([76859.15])
+e_40 = array([79219.01])
+
+
+
+y_roll = [r_10.mean(), r_20.mean(), r_30.mean (), r_40.mean()] #s
+y_event= [e_10.mean(), e_20.mean(), e_30.mean(), e_40.mean()]
+
 y_roll_min = array(y_roll)/60.0
-y_event= [79124.49, 75348.20, 71891.65, 77114.13]
 y_event_min = array(y_event)/60.0
 
 plt.plot(x, y_roll_min, label='rollback', color='green')
