ab13589290e1a6f1227a0756ee02d3abcba705f9
Michele Mattioni <mattioni@ebi.ac.uk>
2012-03-01 09:29:44 +0000

    Added script to plot quickly the calcium in both electrical and bio.

--- helpers/calcium_concentration_plotter.py
+++ helpers/calcium_concentration_plotter.py
@@ -1,22 +1,30 @@
-# IPython log file
+import sys
+import matplotlib.pyplot as plt
 
-get_ipython().magic(u'run nrnvisio Data/26-02-2012/Sim_0/storage.h5')
-get_ipython().magic(u'logstart')
-print "Small volume: 26-02-2012/Sim_0/storage.h5" 
-
-# Biochemical Calcium
-t_559 = controls.manager.groups['timeSeries_spine559']
-t_ms = (t_559.read() - 300) * 1e3
-ca_conc_559 = controls.manager.get_vector('spine559', 'ca_conc', group='timeSeries_spine559')
-plot(t_ms, ca_conc_559.read()*1e6, label="bio")
-xlabel('Time [ms]')
-ylabel('Concentration [um]')
-
-cai_559 = controls.manager.get_vector('spine559_head', 'cai')
-cali_559 = controls.manager.get_vector('spine559_head', 'cali')
-t = controls.manager.groups['t']
-xlabel('Time [ms]')
-ylabel('Concentration [um]')
-
-figure()
-plot(t, ((cali_559.read()+cai_559.read())*1e3), label='electrical calcium')
+def plot_bio_and_ele_calcium(spine, controls):
+	timeSeries = "timeSeries_%s" %spine
+	ecell_time_equilibrium = 300 #s
+	t_bio = controls.manager.groups[timeSeries]
+    	t_bio_ms = (t_bio.read() - ecell_time_equilibrium) * 1e3
+	
+    	ca_conc = controls.manager.get_vector(spine, 'ca_conc', group=timeSeries)
+	label = "bio in %s" %spine
+    	plt.plot(t_bio_ms, ca_conc.read()*1e6, label=label)
+    	plt.title("Bio")
+	plt.xlabel('Time [ms]')
+	plt.ylabel('Concentration [um]')
+        
+	neuron_time_equilibrium = 100 #ms
+        spine_head = "%s_head" %spine
+	cai = controls.manager.get_vector(spine_head, 'cai')
+    	cali = controls.manager.get_vector(spine_head, 'cali')
+	t = controls.manager.groups['t']
+	plt.figure()
+	label = "electrical in %s" %spine
+	plt.plot((t.read()-neuron_time_equilibrium), ((cali.read()+cai.read())*1e3), label=label)
+	plt.xlabel('Time [ms]')
+	plt.ylabel('Concentration [um]')
+    	plt.title("Ele")
+	xlim = plt.xlim(0,) #starting from zero
+	plt.figure(1)
+	plt.xlim(xlim) # Getting bio graph on the electric xlim.
