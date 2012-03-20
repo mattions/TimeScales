91c38832ce2a5930842a94c4f53962e231180b37
Michele Mattioni <mattioni@ebi.ac.uk>
2012-03-20 20:19:25 +0000

    Just saving the freqs plot if we need to go back.

---
+++ helpers/freqs_plot.py
@@ -0,0 +1,26 @@
+# IPython log file
+
+get_ipython().magic(u'logstart ')
+get_ipython().magic(u'pylab')
+from matplotlib import pyplot as plt
+4Hz = [-60, -57]
+4_Hz = [-60, -57]
+freq_4Hz = [-60, -57]
+freq_8Hz = [-60, -56]
+freq_50Hz = [-60, -52]
+abs (-60 - -57)
+abs (-60 - -56)
+abs (-60 - -52)
+x = [4,8,50]
+y = [3,4,8]
+plt.plot(x, y, marker='o', linestyle='-')
+ylim(0, 10)
+ylim(2, 10)
+xlim(0,55)
+ylabel('Delta between trains')
+xlabel('frequencies')
+xlabel('Frequencies')
+ylabel('Delta between trains [mV]')
+xlabel('Frequencies [Hz]')
+plt.savefig('../../../../writings/thesis/gfx/Frequencies_variation.pdf'
+)
