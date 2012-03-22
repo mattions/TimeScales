12c212bde6925d0ff2c68be960b14d588a163bed
Michele Mattioni <mattioni@ebi.ac.uk>
2012-03-22 10:45:13 +0000

    Modified the freqs figure introducing the electrical stimuli.

--- helpers/freqs_plot.py
+++ helpers/freqs_plot.py
@@ -1,10 +1,6 @@
-# IPython log file
 
-get_ipython().magic(u'logstart ')
-get_ipython().magic(u'pylab')
 from matplotlib import pyplot as plt
-4Hz = [-60, -57]
-4_Hz = [-60, -57]
+
 freq_4Hz = [-60, -57]
 freq_8Hz = [-60, -56]
 freq_50Hz = [-60, -52]
@@ -13,14 +9,13 @@ abs (-60 - -56)
 abs (-60 - -52)
 x = [4,8,50]
 y = [3,4,8]
-plt.plot(x, y, marker='o', linestyle='-')
-ylim(0, 10)
-ylim(2, 10)
-xlim(0,55)
-ylabel('Delta between trains')
-xlabel('frequencies')
-xlabel('Frequencies')
-ylabel('Delta between trains [mV]')
-xlabel('Frequencies [Hz]')
-plt.savefig('../../../../writings/thesis/gfx/Frequencies_variation.pdf'
-)
+plt.plot(x, y, marker='o', linestyle='-', label='ele_and_bio')
+y_1 = [0,0,0]
+plt.plot(x, y_1, marker='o', linestyle='-', label="only_ele")
+plt.xlim(0,55)
+plt.ylim(-0.5, 10)
+
+plt.ylabel('Delta between trains [mV]')
+plt.xlabel('Frequencies [Hz]')
+plt.legend(loc=0)
+#plt.savefig('../../../../writings/thesis/gfx/Frequencies_variation.pdf')
